40622857ad129b35e3599b4fb17ecd93578dac12
Nicholas <forksez.temp@gmail.com>
2025-02-07 19:23:14 +0100

    Updated /kick: administrator privileges check

--- cogs/kick.py
+++ cogs/kick.py
@@ -6,9 +6,19 @@ class KickCommand(commands.Cog):
         self.bot = bot
 
     @commands.slash_command(description = "Кикнуть участника")
+    @commands.has_permissions(administrator=True)
     async def kick(self, inter, member: disnake.Member, reason: str = commands.Param("не указана")):
+        if member.top_role >= inter.author.top_role:
+            return await inter.response.send_message("Вы не можете кикнуть данного участника.")
+        await member.send(f"Вы были кикнуты с сервера {inter.guild.name} по причине: **{reason}**.")
         await member.kick(reason=reason)
         await inter.response.send_message(f"Пользователь **{member}** был кикнут по причине: **{reason}**.")
+
+    @kick.error
+    async def kick_error(self, inter, error):
+        if isinstance(error, commands.MissingPermissions):
+            await inter.response.send_message("У вас нет прав администратора для выполнения этой команды.",
+                                              ephemeral=True)
 
 def setup(bot):
     bot.add_cog(KickCommand(bot))
