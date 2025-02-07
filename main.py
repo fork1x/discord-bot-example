732d0416d1f422f40b3a6cca5180cf98ca74bac3
Nicholas <forksez.temp@gmail.com>
2025-02-07 13:03:43 +0100

    Refactor: Automate loading of cogs from 'cogs'

--- main.py
+++ main.py
@@ -1,15 +1,14 @@
 import disnake
+import os
 from disnake.ext import commands
 
 bot = commands.Bot(command_prefix="!", intents=disnake.Intents.all(), owner_id="537976988573368323",
-                   activity=disnake.Game("/help", status=disnake.Status.dnd, description="Описание"))
+                   activity=disnake.Game("/help - помощь", status=disnake.Status.online))
 bot.remove_command("help")
 
-bot.load_extension("cogs.help")
-bot.load_extension("cogs.server_info")
-bot.load_extension("cogs.events")
-bot.load_extension("cogs.clear")
-bot.load_extension("cogs.embed")
+for filename in os.listdir("cogs"):
+    if filename.endswith(".py"):
+        bot.load_extension(f"cogs.{filename[:-3]}")
 
 
 token = open("token.txt", "r").readline()
